43d2e5f21e2a21ec41ee86bad2ae7bb4322e64a8
bhojani155-pixel <bhojanigopal155@gmail.com>
2026-08-07 23:50:08 +0530

    OneSignal Added

--- OneSignalSDKWorker.js
+++ OneSignalSDKWorker.js
@@ -1,1 +1,8 @@
-importScripts('https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js');
+window.OneSignalDeferred = window.OneSignalDeferred || [];
+OneSignalDeferred.push(async function(OneSignal) {
+  await OneSignal.init({
+    appId: "9278cc17-9628-42ef-ace6-cbef8e03f779",
+    // 👇 यह लाइन ज़रूर जोड़ें
+    serviceWorkerPath: "OneSignalSDKWorker.js",
+  });
+});
